e2126b472ddf9709a3557dd51149241199ed2360
Ursila Pradeep <68369914+ursilapradeep@users.noreply.github.com>
2025-07-04 06:29:27 +0000

    test_abs_val

---
+++ c_piscine_re/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "ft_abs.h"
+
+int main()
+{
+    int n1 = -3;
+    int n2 = 5;
+
+    printf("ABS(%d) = %d\n", n1, ABS(n1));
+    printf("ABS(%d) = %d\n", n2, ABS(n2));
+    return 0;
+}
